b3609f41b2b9a00b6170fb331f6d073c97823d84
mgallegoa <manuelgallego2003@yahoo.com>
2025-07-21 13:29:00 -0500

    HOTEL-SPRRING : Run test in command, run the repository test in spring boot.

--- src/main/java/com/co/manuel/hotel_spring/HotelSpringApplication.java
+++ src/main/java/com/co/manuel/hotel_spring/HotelSpringApplication.java
@@ -1,13 +1,32 @@
 package com.co.manuel.hotel_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.co.manuel.hotel_spring.model.Guest;
+import com.co.manuel.hotel_spring.repository.GuestRepository;
 
 @SpringBootApplication
 public class HotelSpringApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HotelSpringApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunnerTestRepository(GuestRepository repository) {
+    return args -> {
+      var guest = Guest.builder()
+          .firstName("Manuel")
+          .lastName("Test from command line")
+          .birthDay("August")
+          .nationality("Col")
+          .phone("311730")
+          .build();
+      repository.save(guest);
+    };
   }
 
 }
