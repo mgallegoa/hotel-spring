79b177d09f813dfed760b126116045b7b284f8e3
mgallegoa <manuelgallego2003@yahoo.com>
2025-08-04 15:39:05 -0500

    HOTEL-SPRRING : TEST : Add test for Reservation Repository.

---
+++ src/test/java/com/co/manuel/hotel_spring/repository/ReservationRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.co.manuel.hotel_spring.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.co.manuel.hotel_spring.model.Reservation;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ReservationRepositoryTest {
+
+  @Autowired
+  ReservationRepository reservationRepository;
+
+  @Test
+  public void ReservationRepository_Save_ReturnSaved() {
+    // Arrange
+    Reservation reservation = Reservation.builder().dateIn("06062025").dateOut("06072025").costToPay(300)
+        .payUsed("credit card").build();
+
+    // Act
+    Reservation savedReservation = reservationRepository.save(reservation);
+
+    // Assertion
+    Assertions.assertThat(savedReservation).isNotNull();
+    Assertions.assertThat(savedReservation.getIdReservation()).isEqualTo(1L);
+    Assertions.assertThat(savedReservation.getDateIn()).isEqualTo(reservation.getDateIn());
+
+  }
+
+}
