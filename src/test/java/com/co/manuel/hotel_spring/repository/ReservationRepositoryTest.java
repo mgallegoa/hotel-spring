ac7be0a5e78121ec274d47db65ce757ff2165414
mgallegoa <manuelgallego2003@yahoo.com>
2025-08-04 18:38:46 -0500

    HOTEL-SPRRING : TEST : Add test for Reservation Repository, method deleteById.

--- src/test/java/com/co/manuel/hotel_spring/repository/ReservationRepositoryTest.java
+++ src/test/java/com/co/manuel/hotel_spring/repository/ReservationRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.co.manuel.hotel_spring.repository;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,20 @@ public class ReservationRepositoryTest {
     Assertions.assertThat(savedReservation).isNotNull();
     Assertions.assertThat(savedReservation.getIdReservation()).isEqualTo(1L);
     Assertions.assertThat(savedReservation.getDateIn()).isEqualTo(reservation.getDateIn());
+
+  }
+
+  @Test
+  public void ReservationRepository_Delete_ReturnVoid() {
+    // Arrange
+    Reservation reservation = Reservation.builder().dateIn("06062025").dateOut("06072025").costToPay(300)
+        .payUsed("credit card").build();
+
+    // Act
+    Reservation savedReservation = reservationRepository.save(reservation);
+
+    // Assertion
+    assertAll(() -> reservationRepository.deleteById(savedReservation.getIdReservation()));
 
   }
 
