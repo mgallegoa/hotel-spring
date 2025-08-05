6c6515ee9b11b22351fe6b0654845540c121e10b
mgallegoa <manuelgallego2003@yahoo.com>
2025-08-05 10:49:40 -0500

    HOTEL-SPRRING : TEST : Add test profile and use in Guest repository.

--- src/test/java/com/co/manuel/hotel_spring/repository/ReservationRepositoryTest.java
+++ src/test/java/com/co/manuel/hotel_spring/repository/ReservationRepositoryTest.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.co.manuel.hotel_spring.model.Reservation;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@ActiveProfiles("test")
 public class ReservationRepositoryTest {
 
   @Autowired
