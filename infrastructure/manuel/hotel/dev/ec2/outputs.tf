8171554d02e398523be0b71189bcc2767d3b574e
mgallegoa <manuelgallego2003@yahoo.com>
2025-09-12 22:24:44 -0500

    HOTEL-SPRRING : INFRASTRUCTURE : Add output to show the public ip.

--- infrastructure/manuel/hotel/dev/ec2/outputs.tf
+++ infrastructure/manuel/hotel/dev/ec2/outputs.tf
@@ -6,3 +6,8 @@ output "instance_ip_private-subnet" {
   value = aws_instance.hotel-instances-private-subnet.private_ip
   # value = { for service, i in aws_instance.hotel-instances : service => i.private_ip }
 }
+
+output "hotel-instances-public-subnet_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.hotel-instances-public-subnet.public_ip
+}
